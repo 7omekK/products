d3a6444900afce9560f9f72856f455e26130436b
Fullstack <fullstack@MacBook-Pro-Admin.local>
2022-08-08 23:47:20 +0200

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1 className="text-3xl font-bold">
+          React app with tailwind css works!
+        </h1>
       </header>
-    </div>
+      <main></main>
+    </>
   );
 }
 
